c9e40bed9e42fddfc99d323bca129fbdcc97f9c8
Yasutaka OHMURA <ohmura.yasutaka@classmethod.jp>
2023-10-08 09:45:16 +0900

    Initial commit

---
+++ test/test-sorry-page.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestSorryPage from '../lib/test-sorry-page-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test-sorry-page-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestSorryPage.TestSorryPageStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
